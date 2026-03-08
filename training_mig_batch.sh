ab18f84ef6fe70fb4934a36beae5c1ec01742bb6
MadoctheHadoc <madockarthaus@hotmail.com>
2026-03-08 16:26:53 +0100

    Forgot transformers

--- training_mig_batch.sh
+++ training_mig_batch.sh
@@ -20,6 +20,7 @@ pip install --upgrade pip
 
 pip install torch==2.5.1+cu121 --index-url https://download.pytorch.org/whl/cu121
 pip install tqdm
+pip install transformer
 pip install lerobot==0.3.3
 pip install -e .
 
