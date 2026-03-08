b991d7877369be81f651c36abe14f07ff132d527
MadoctheHadoc <madockarthaus@hotmail.com>
2026-03-08 13:58:18 +0100

    Update training batch script
    Update training batch script for isaacsim, isaaclab and torch

--- training_mig_batch.sh
+++ training_mig_batch.sh
@@ -17,9 +17,19 @@ module load CUDA/12.1.1
 source venv/bin/activate
 
 pip install --upgrade pip
-# torch being funny and needing special treatment
+
+# Install torch first (needs special index)
 pip install torch==2.5.1+cu121 --index-url https://download.pytorch.org/whl/cu121
-pip install -r reqs.txt --extra-index-url https://download.pytorch.org/whl/cu121
+
+# Install everything from reqs except isaaclab and isaacsim (handled separately)
+grep -vE "isaaclab|isaacsim" reqs.txt > reqs_filtered.txt
+pip install -r reqs_filtered.txt --extra-index-url https://download.pytorch.org/whl/cu121
+
+# Install isaacsim from NVIDIA's index
+pip install isaacsim==5.1.0.0 --extra-index-url https://pypi.nvidia.com/
+
+# Install isaaclab from source
+pip install git+https://github.com/isaac-sim/IsaacLab.git@v2.3.2.post1
 
 python vbti/utils/train/train_smolvla_custom.py
 deactivate
