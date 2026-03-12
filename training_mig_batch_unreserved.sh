416d41f3c5ca7f2fa02a3f6858feea4c788b53aa
MadoctheHadoc <madockarthaus@hotmail.com>
2026-03-12 21:08:12 +0100

    ,

--- training_mig_batch_unreserved.sh
+++ training_mig_batch_unreserved.sh
@@ -22,6 +22,7 @@ pip install --upgrade pip
 pip install torch==2.5.1+cu121 --index-url https://download.pytorch.org/whl/cu121
 pip install tqdm
 pip install lerobot
+pip install --upgrade huggingface_hub
 pip install -e .
 
 python vbti/utils/train/train_smolvla_custom.py
